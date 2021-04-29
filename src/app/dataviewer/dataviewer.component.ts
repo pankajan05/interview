71d15787143ad398633998161f30acd8d5120f14
pankajan <pankajan05@gmail.com>
2021-04-29 23:28:26 +0530

    add the data

--- src/app/dataviewer/dataviewer.component.ts
+++ src/app/dataviewer/dataviewer.component.ts
@@ -6,7 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dataviewer.component.css']
 })
 export class DataviewerComponent implements OnInit {
-  items: any;
+  items = [
+    {size: '7500', category: 'fitness equipement', Employee: 'dilshan', location: 'purchasing', pipeline: 'ezuitesale', stage: 'ProductDemo', lastupdate: '2020-12-03 04:23:43'},
+    {size: '7500', category: 'fitness equipement', Employee: 'dilshan', location: 'purchasing', pipeline: 'ezuitesale', stage: 'ProductDemo', lastupdate: '2020-12-03 04:23:43'},
+    {size: '7500', category: 'fitness equipement', Employee: 'dilshan', location: 'purchasing', pipeline: 'ezuitesale', stage: 'ProductDemo', lastupdate: '2020-12-03 04:23:43'},
+    {size: '7500', category: 'fitness equipement', Employee: 'dilshan', location: 'purchasing', pipeline: 'ezuitesale', stage: 'ProductDemo', lastupdate: '2020-12-03 04:23:43'},
+    {size: '7500', category: 'fitness equipement', Employee: 'dilshan', location: 'purchasing', pipeline: 'ezuitesale', stage: 'ProductDemo', lastupdate: '2020-12-03 04:23:43'},
+    {size: '7500', category: 'fitness equipement', Employee: 'dilshan', location: 'purchasing', pipeline: 'ezuitesale', stage: 'ProductDemo', lastupdate: '2020-12-03 04:23:43'},
+    {size: '7500', category: 'fitness equipement', Employee: 'dilshan', location: 'purchasing', pipeline: 'ezuitesale', stage: 'ProductDemo', lastupdate: '2020-12-03 04:23:43'},
+    {size: '7500', category: 'fitness equipement', Employee: 'dilshan', location: 'purchasing', pipeline: 'ezuitesale', stage: 'ProductDemo', lastupdate: '2020-12-03 04:23:43'},
+    {size: '7500', category: 'fitness equipement', Employee: 'dilshan', location: 'purchasing', pipeline: 'ezuitesale', stage: 'ProductDemo', lastupdate: '2020-12-03 04:23:43'},
+    {size: '7500', category: 'fitness equipement', Employee: 'dilshan', location: 'purchasing', pipeline: 'ezuitesale', stage: 'ProductDemo', lastupdate: '2020-12-03 04:23:43'},
+  ];
 
   constructor() { }
 
