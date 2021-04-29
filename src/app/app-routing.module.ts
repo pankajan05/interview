d7d42b0b6d0a4c81a45c62416511fcc56f5b037b
pankajan <pankajan05@gmail.com>
2021-04-29 21:55:02 +0530

    work navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import {HomeComponent} from './home/home.component';
 import {DataviewerComponent} from './dataviewer/dataviewer.component';
 
 const routes: Routes = [
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
   { path: 'home', component: HomeComponent },
   { path: 'data', component: DataviewerComponent }
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,18 @@ import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { HomeComponent } from './home/home.component';
 import { DataviewerComponent } from './dataviewer/dataviewer.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
     HomeComponent,
-    DataviewerComponent
+    DataviewerComponent,
+    FooterComponent
+  ],
+  exports: [
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
